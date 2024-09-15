771cb9630f91985ef7c18bf4664fc0fdeb326e65
Rahul Jamba <rahuljamba.exe@gmail.com>
2024-09-16 00:49:59 +0530

    Upload sample code

---
+++ BlueMac/BlueMacApp.swift
@@ -0,0 +1,20 @@
+//
+//  BlueMacApp.swift
+//  BlueMac
+//
+//  Created by Admin on 08/09/24.
+//
+
+
+import SwiftUI
+
+@main
+struct BlueMacApp: App {
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
